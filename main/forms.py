b8f4fd3afc71c7b1778908ec2c15560a256590d8
hizounya <hizounyamaycinto@gmail.com>
2024-10-09 12:00:49 +0700

    Tugas 6

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,25 @@
 from django import forms  
-from main.models import ItemEntry  
+from main.models import ItemEntry
+from django.utils.html import strip_tags
 
 class ItemForm(forms.ModelForm):  # This should be from 'forms.ModelForm'
     class Meta:
         model = ItemEntry
         fields = ['nama_item', 'harga', 'deskripsi', 'size', 'warna', 'jumlah']  # Include your model fields here
+
+    def clean_nama_item(self):
+        nama_item = self.cleaned_data["nama_item"]
+        # Remove any HTML tags to ensure clean input
+        return strip_tags(nama_item)
+
+    def clean_deskripsi(self):
+        deskripsi = self.cleaned_data["deskripsi"]
+        # Remove any HTML tags to ensure clean input
+        return strip_tags(deskripsi)
+
+    def clean_harga(self):
+        harga = self.cleaned_data["harga"]
+        # Ensure the price is a positive value
+        if harga < 0:
+            raise forms.ValidationError("Harga cannot be negative.")
+        return harga
--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 from main.views import show_main, create_item_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register
-from main.views import login_user
-from main.views import logout_user
-from main.views import edit_item
-from main.views import delete_item
+from main.views import register, login_user,logout_user
+from main.views import edit_item, delete_item, add_item_entry_ajax
+from . import views
 
-app_name = 'main' 
-
+app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-item-entry/',create_item_entry, name='create_item_entry'),
-    path('edit-item/<uuid:id>/',edit_item, name='edit_item'),
-    path('delete/<uuid:id>', delete_item, name='delete_item'),
+    path('create-item-entry/', create_item_entry, name='create_item_entry'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
@@ -20,4 +15,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-item/<uuid:id>', edit_item, name='edit_item'),
+    path('delete/<uuid:id>', delete_item, name='delete_item'),
+    path('create-item-entry-ajax', add_item_entry_ajax, name='add_item_entry_ajax'),
 ]
