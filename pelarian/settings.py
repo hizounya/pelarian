ff006f85bba14795b29a9953d22c4204850a7524
hizounya <hizounyamaycinto@gmail.com>
2024-11-20 01:19:45 +0700

    nambahin authentication

--- pelarian/settings.py
+++ pelarian/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-t3yl6^%t@rx-5)#jol8cuetv_s^z-$a%70k7okl1l^hy&gz34+
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://hizounya-maycinto-pelarian.pbp.cs.ui.ac.id", "https://hizounya-maycinto-pelarian.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://hizounya-maycinto-pelarian.pbp.cs.ui.ac.id", "https://hizounya-maycinto-pelarian.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main'
+    'authentication'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'pelarian.urls'
@@ -134,3 +137,10 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
